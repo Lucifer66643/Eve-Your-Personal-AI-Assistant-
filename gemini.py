c376b16bf6375f6796938d83e257da62614738a5
Sujit Khade <111680306+Lucifer66643@users.noreply.github.com>
2025-03-19 01:50:34 +0530

    (Without API key)

--- gemini.py
+++ gemini.py
@@ -2,7 +2,7 @@ import google.generativeai as genai
 
 class Gemini:
     def __init__(self,  model_name):
-        genai.configure(api_key="AIzaSyDc1-k7bQSFA2yIr7Z-4pCAVn57nyr0drs")
+        genai.configure(api_key="API_key")
         self.model = genai.GenerativeModel(model_name)
 
     def generate_code(self, prompts):
